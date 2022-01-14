8ed792bb900d02af553cfc553b5c0561a59fcba8
goldenslumber <goldenslumbers0902@gmail.com>
2022-01-14 14:40:31 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,5 @@
 
 <x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
 <x-tests.card title="タイトル"></x-tests.card>
+<x-tests.card title="CSSを変更したい" class="bg-red-300"></x-tests.card>
 </x-tests.app>
