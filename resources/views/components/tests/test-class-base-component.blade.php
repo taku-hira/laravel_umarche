664951d6f065ba9dc1a7d9a8616e18044620fe5a
goldenslumber <goldenslumbers0902@gmail.com>
2022-01-14 14:58:12 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+
+    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,9 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
-コンポーネント１
+コンポーネント2
+
+<x-test-class-base classBaseMessage></x-test-class-base>
+
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage></x-test-class-base>
+
 </x-tests.app>
