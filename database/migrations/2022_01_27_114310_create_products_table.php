6b7f1019745e1e2e91875370b510041b3e2ef8ac
goldenslumber <goldenslumbers0902@gmail.com>
2022-01-27 16:29:27 +0900

    productCreate

--- database/migrations/2022_01_27_114310_create_products_table.php
+++ database/migrations/2022_01_27_114310_create_products_table.php
@@ -15,12 +15,20 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('price');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
                 $table->foreignId('secondary_category_id')->constrained();
                 $table->foreignId('image1')->nullable()->constrained('images');
+                $table->foreignId('image2')->nullable()->constrained('images');
+                $table->foreignId('image3')->nullable()->constrained('images');
+                $table->foreignId('image4')->nullable()->constrained('images');
             $table->timestamps();
         });
     }
