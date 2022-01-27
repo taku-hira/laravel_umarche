48a0cb3535a4ced35b1b3c373f287072e7e7d4f6
goldenslumber <goldenslumbers0902@gmail.com>
2022-01-27 13:45:46 +0900

    eager Loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -3,10 +3,36 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Image;
+use App\Models\secondaryCategory;
+use App\Models\Owner;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            // dd($request->route()->parameter('shop')); //文字列
+            // dd(Auth::id()); // 数字型
+            $id = $request->route()->parameter('product'); //shopのid取得
+            if(!is_null($id)){ // null判定
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId; // キャスト 文字列→数値に型変換
+                if($productId !== Auth::id()){ // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +40,21 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+        ->where('id', Auth::id())->get();
+
+        // dd($ownerInfo);
+
+        // foreach ($ownerInfo as $owner) {
+        //     // dd($owner->shop->product);
+        //     foreach ($owner->shop->product as $product) {
+        //         dd($product->imageFirst->name);
+        //     }
+        // }
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
