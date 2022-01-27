8790ef5a04639001106963f106c622f3dd76889f
goldenslumber <goldenslumbers0902@gmail.com>
2022-01-28 01:34:44 +0900

    productEdit

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -41,13 +41,26 @@
                                     <div class="relative">
                                         <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫</label>
                                         <input type="hidden" id="current_quantity" name="current_quantity" value="{{ $quantity }}">
-                                        <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $quantity }}</div>
+                                        <div class="w-full bg-gray-100 bg-opacity-50 rounded text-base outline-none text-gray-700 py-1 px-3 leading-8">{{ $quantity }}</div>
+                                    </div>
+                                </div>
+                                <div class="p-2 w-1/2 mx-auto">
+                                    <div class="relative flex justify-around">
+                                        <div>
+                                            <input type="radio" name="type" value="1" class="mr-2" checked>
+                                            追加
+                                        </div>
+                                        <div>
+                                            <input type="radio" name="type" value="2" class="mr-2">
+                                            削減
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="p-2 w-1/2 mx-auto">
                                     <div class="relative">
-                                        <label for="quantity" class="leading-7 text-sm text-gray-600">初期在庫 ＊必須</label>
-                                        <input type="number" id="quantity" name="quantity" value="{{ old('quantity') }}" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        <label for="quantity" class="leading-7 text-sm text-gray-600">数量</label>
+                                        <input type="number" id="quantity" name="quantity" value="0" required class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        <span class="text-sm">0~99の範囲で入力してください</span>
                                     </div>
                                 </div>
                                 <div class="p-2 w-1/2 mx-auto">
@@ -55,7 +68,7 @@
                                         <label for="shop_id" class="leading-7 text-sm text-gray-600">販売店舗</label>
                                         <select name="shop_id" id="shop_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                             @foreach ($shops as $shop)
-                                                <option value="{{ $shop->id }}">
+                                                <option value="{{ $shop->id }}" @if ($shop->id === $product->shop->id) selected @endif>
                                                     {{ $shop->name }}
                                                 </option>
                                             @endforeach
@@ -69,7 +82,7 @@
                                             @foreach($categories as $category)
                                                 <optgroup label="{{ $category->name }}">
                                                 @foreach($category->secondary as $secondary)
-                                                    <option value="{{ $secondary->id}}" >
+                                                    <option value="{{ $secondary->id}}" @if ($secondary->id === $product->secondary_category_id) selected @endif >
                                                         {{ $secondary->name }}
                                                     </option>
                                                 @endforeach
@@ -77,26 +90,34 @@
                                         </select>
                                     </div>
                                 </div>
-                                <x-select-image :images="$images" name="image1" />
-                                <x-select-image :images="$images" name="image2" />
-                                <x-select-image :images="$images" name="image3" />
-                                <x-select-image :images="$images" name="image4" />
+                                <x-select-image :images="$images" currentId="{{$product->image1}}"
+                                currentImage="{{$product->imageFirst->name ?? ''}}" name="image1" />
+
+                                <x-select-image :images="$images" currentId="{{$product->image1}}"
+                                    currentImage="{{$product->imageSecond->name ??''}}" name="image2" />
+
+                                <x-select-image :images="$images" currentId="{{$product->image1}}"
+                                    currentImage="{{$product->imageThird->name ??''}}" name="image3" />
+
+                                <x-select-image :images="$images" currentId="{{$product->image1}}"
+                                    currentImage="{{$product->imageFourth->name ??''}}" name="image4" />
+
                                 <x-select-image :images="$images" name="image5" />
                                 <div class="p-2 w-1/2 mx-auto">
                                     <div class="relative flex justify-around">
                                         <div>
-                                            <input type="radio" name="is_selling" value="1" class="mr-2" checked>
+                                            <input type="radio" name="is_selling" value="1" class="mr-2" @if($product->is_selling === 1){ checked } @endif>
                                             販売中
                                         </div>
                                         <div>
-                                            <input type="radio" name="is_selling" value="0" class="mr-2">
+                                            <input type="radio" name="is_selling" value="0" class="mr-2" @if($product->is_selling === 0){ checked } @endif>
                                             停止中
                                         </div>
                                     </div>
                                 </div>
                                 <div class="p-2 w-full flex justify-around mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新</button>
                                 </div>
                             </div>
                     </form>
